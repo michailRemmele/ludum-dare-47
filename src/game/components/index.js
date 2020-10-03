aeeed6701bb020f7b8e7a97c48f6a24a334969e8
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-10-03 15:40:20 +0600

    added melee combat system

--- src/game/components/index.js
+++ src/game/components/index.js
@@ -1,7 +1,9 @@
 import Health from './health/health';
 import HitBox from './hitBox/hitBox';
+import MeleeWeapon from './meleeWeapon/meleeWeapon';
 
 export default {
   health: Health,
   hitBox: HitBox,
+  meleeWeapon: MeleeWeapon,
 };
--- src/game/processorsPlugins/index.js
+++ src/game/processorsPlugins/index.js
@@ -2,10 +2,13 @@ import DamageProcessorPlugin from './damageProcessorPlugin/damageProcessorPlugin
 import EffectsProcessorPlugin from './effectsProcessorPlugin/effectsProcessorPlugin';
 import GameOverProcessorPlugin from './gameOverProcessorPlugin/gameOverProcessorPlugin';
 import ReaperPlugin from './reaperPlugin/reaperPlugin';
+import FightProcessorPlugin from './fightProcessorPlugin/fightProcessorPlugin';
+
 
 export default {
   damageProcessor: DamageProcessorPlugin,
   effectsProcessor: EffectsProcessorPlugin,
   gameOverProcessor: GameOverProcessorPlugin,
   reaper: ReaperPlugin,
+  fightProcessor: FightProcessorPlugin,
 };
