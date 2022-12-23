88b969f9934dab5d1ecb947e689c8574069e903e
Mikhail Remmele <mikhail.remmele@gmail.com>
2022-12-23 20:09:46 +0300

    add custom widget for day night simulator system

--- editor/schema/systems/day-night-simulator/index.js
+++ editor/schema/systems/day-night-simulator/index.js
@@ -1,3 +1,24 @@
+import { DayNightSimulatorWidget } from './view';
+
 export const dayNightSimulator = {
   title: 'systems.dayNightSimulator.title',
+  fields: [
+    {
+      name: 'dayLength',
+      title: 'systems.dayNightSimulator.dayLength.title',
+      type: 'number',
+    },
+    {
+      name: 'startTime',
+      title: 'systems.dayNightSimulator.startTime.title',
+      type: 'number',
+    },
+    {
+      name: 'skyId',
+      title: 'systems.dayNightSimulator.skyId.title',
+      type: 'select',
+      referenceId: 'gameObjects',
+    },
+  ],
+  view: DayNightSimulatorWidget,
 };
---
+++ editor/schema/systems/day-night-simulator/view.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Widget,
+  EngineContext,
+  get,
+} from 'remiz-editor';
+
+const SCENE_PATH_LENGTH = 2;
+
+const getItems = (
+  gameObjects,
+  parent,
+) => gameObjects.reduce((acc, gameObject) => {
+  acc.push({
+    title: parent ? `${parent.name}.${gameObject.name}` : gameObject.name,
+    value: gameObject.id,
+  });
+
+  if (gameObject.children?.length) {
+    acc.push(...getItems(gameObject.children, gameObject));
+  }
+
+  return acc;
+}, []);
+
+export const DayNightSimulatorWidget = ({ fields, path, references }) => {
+  const { sceneContext } = useContext(EngineContext);
+  const projectConfig = sceneContext.data.projectConfig;
+
+  const items = useMemo(() => {
+    const scene = get(projectConfig, path.slice(0, SCENE_PATH_LENGTH));
+
+    if (typeof scene.level !== 'string') {
+      return [];
+    }
+
+    const { gameObjects } = get(projectConfig, [ 'levels', scene.level ]);
+
+    return getItems(gameObjects);
+  }, [ projectConfig ]);
+
+  const extReferences = useMemo(() => ({
+    ...references,
+    gameObjects: {
+      items,
+    },
+  }), [ references ]);
+
+  return (
+    <Widget path={path} fields={fields} references={extReferences} />
+  );
+};
+
+DayNightSimulatorWidget.propTypes = {
+  fields: PropTypes.object,
+  path: PropTypes.arrayOf(PropTypes.string),
+  references: PropTypes.object,
+};
