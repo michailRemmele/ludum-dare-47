2d231053e9f82e524b9995d8c52d5487281fd7e8
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-09-11 19:58:35 +0300

    update editor version

--- remiz-editor.config.js
+++ remiz-editor.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   projectConfig: 'public/resources/config.json',
   assets: 'public',
-  extension: 'build-editor/index.js',
+  extensionEntry: 'editor/index.js',
   autoSave: true,
   autoSaveInterval: 5_000,
 };
